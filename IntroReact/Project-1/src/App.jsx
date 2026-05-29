c68c593c652a41a61470a8bc0f35f70a7ddd90c6
sohamaggarwal007 <sohamaggarwal007@gmail.com>
2026-05-29 18:48:09 +0530

    chore: restore IntroReact folder from previous commit

---
+++ IntroReact/Project-1/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>Hi, My name is Soham Aggarwal</h1>
+    </>
+  )
+}
+
+export default App
---
+++ IntroReact/Project-1/src/componenets/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>I am a Nav Bar</div>
+    </div>
+  )
+}
+
+export default Navbar
